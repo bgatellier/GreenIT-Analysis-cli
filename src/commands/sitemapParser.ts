6a2f5d902430935dda90bb4a58d5b575103850eb
Bastien Gatellier <contact@bgatellier.fr>
2026-02-21 20:18:35 +0100

    chore: move sitemapParser to TS

--- src/commands/sitemapParser.ts
+++ src/commands/sitemapParser.ts
@@ -1,10 +1,18 @@
-const Sitemapper = require('sitemapper');
-const fs = require('fs');
-const YAML = require('yaml');
-const path = require('path');
-const sitemap = new Sitemapper();
+import fs from 'node:fs';
+import path from 'node:path';
+import Sitemapper from 'sitemapper';
+import YAML from 'yaml';
 
-module.exports = (options) => {
+type Options = {
+    sitemap_url: string,
+    yaml_output_file: string,
+}
+
+const sitemap = new Sitemapper({
+    timeout: 10000, // 10 second timeout
+});
+
+export default function parse(options: Options) {
     //handle inputs
     const SITEMAP_URL = options.sitemap_url;
     const OUTPUT_FILE = path.resolve(options.yaml_output_file);
@@ -22,4 +30,4 @@ module.exports = (options) => {
             }
         })
         .catch((e) => console.log('ERROR : \n' + e));
-};
+}
